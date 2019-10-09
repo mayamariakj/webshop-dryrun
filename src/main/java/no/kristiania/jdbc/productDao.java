1647ea31d605717526ffd3cf029f002f6ac08276
mayamariakjaer <maya_t_kjaer@hotmail.com>
2019-10-09 16:32:00 +0200

    add .travis.yml file ny editor

--- src/main/java/no/kristiania/jdbc/productDao.java
+++ src/main/java/no/kristiania/jdbc/productDao.java
@@ -1,16 +1,37 @@
 package no.kristiania.jdbc;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class productDao {
 
     private List<String> products = new ArrayList<>();
+    private DataSource dataSource;
+
+    public productDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
 
     public void insertProduct(String productName) {
         products.add(productName);
+
+        try (Connection conn = dataSource.getConnection()){
+            PreparedStatement statement = conn.prepareStatement
+                    ("insert into products(name) values(?)"
+            );
+            statement.setString(1, productName);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
+
 
     public List<String> listAll() {
         return products;
--- src/test/java/no/kristiania/jdbc/WebshopTest.java
+++ src/test/java/no/kristiania/jdbc/WebshopTest.java
@@ -1,15 +1,25 @@
 package no.kristiania.jdbc;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.Random;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class WebshopTest {
+
     @Test
-    void shouldRetrieveStoredProduct(){
-        productDao dao = new productDao();
+    void shouldRetrieveStoredProduct() throws SQLException {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:test");
+
+        dataSource.getConnection().createStatement().executeUpdate(
+                "create table products(name varchar (100))"
+        );
+
+        productDao dao = new productDao(dataSource);
         String productName = pickOne(new String[]{ "Apples", "Bananas", "Coconuts", "Dates"});
         dao.insertProduct(productName);
         assertThat(dao.listAll()).contains(productName);
