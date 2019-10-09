feffb9575ab782c6ab30c85c87878d68a1e417be
mayamariakjaer <maya_t_kjaer@hotmail.com>
2019-10-09 12:42:59 +0200

    Initial commit
    *productDao
    *webShopTest

---
+++ src/test/java/no/kristiania/jdbc/WebshopTest.java
@@ -0,0 +1,20 @@
+package no.kristiania.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WebshopTest {
+    @Test
+    void shouldRetrieveStoredProduct(){
+        productDao dao = new productDao();
+        String productName = pickOne(new String[]{ "Apples", "Bananas", "Coconuts", "Dates"});
+        dao.insertProduct(productName);
+        assertThat(dao.listAll()).contains(productName);
+    }
+    private String pickOne(String[] strings){
+        return strings[new Random().nextInt(strings.length)];
+    }
+}
